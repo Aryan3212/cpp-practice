931e0a86d15aab1499e4785b830af57f5c62a7bc
Aryan Rahman <57558971+Aryan3212@users.noreply.github.com>
2026-07-19 15:22:29 +0600

    c++ templates and practice

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
-// #include <math.h>
 #include <string.h>
 #include<iostream>
+#include <algorithm>
 // #include<bits/stdc++.h> This is a header file that is only available on GNU GCC compiler.
 // algorithms, containers, functions, iterators
 // using namespace std;
@@ -41,6 +41,7 @@ void pattern3(int n ) {
     std::cout << "\n";
   }
 }
+
 void pattern4(int n ) {
   for (int i = 0; i < n; i++) {
     for (int j = 0; j <= i; j++) {
@@ -360,6 +361,27 @@ void pattern19(int n) {
     std::cout << "\n";
   }
 }
+void pattern19_gridzero_based(int n) {
+  int size = 2 * n; // size of the diamond in length
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size; j++) {
+      int leftSide, rightSide;
+      if (i < n) {
+        leftSide = n - i;
+        rightSide = n + i; // 5,6,7,8,9
+      } else {
+        leftSide = i - n + 1;
+        rightSide = size - i + n - 1; // 9,8,7,6,5 // 10,5, 
+      }
+      if (j < leftSide || j >= rightSide) {
+        std::cout << "*";
+      } else {
+        std::cout << "-";
+      }
+    }
+    std::cout << "\n";
+  }
+}
 void pattern19_pointydiamond(int n) {
   int size = 2 * n; // size of the diamond in length
   for (int i = 1; i <= size; i++) {
@@ -381,6 +403,75 @@ void pattern19_pointydiamond(int n) {
     std::cout << "\n";
   }
 }
+void pattern20_gridzero_based(int n) {
+  int size = 2 * n - 1; // size of the diamond in length
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size + 1; j++) {
+      int leftSide, rightSide;
+      if (i < n) {
+        leftSide = i;
+        rightSide = 2 * n - i - 1; // 5,6,7,8,9
+      } else {
+        leftSide = 2 * n - i - 2;
+        rightSide = i + 1; // 9,8,7,6,5 // 10,5, 
+      }
+      if (j <= leftSide || j >= rightSide) {
+        std::cout << "*";
+      } else {
+        std::cout << "-";
+      }
+    }
+    std::cout << "\n";
+  }
+}
+ 
+template<typename F>
+void pattern17_practice_numbers_version(int n, F f) {
+  for (int i = 0; i < n; i++) {
+    int spaces = n - i - 1; // 3
+    int letters =  2 * i + 1;
+    for (int j = 0; j < spaces; j++) {
+      std::cout << " ";
+    }
+    // 1, 121, 12321, 1234321
+    for (int j = 1; j <= letters; j++) {
+      int mid = i + 1;
+      std::cout << f(mid - abs(mid - j)); // mid - distance from mid
+    }
+    std::cout << "\n";
+  }
+}
+//    1     0 1
+//   121    1 2
+//  12321   2 3
+// 1234321  3 4
+void pattern17_practice_numbers_version(int n) {
+    pattern17_practice_numbers_version(n, [](int x) {
+        return x;
+    });
+}
+template<typename F>
+void pattern17_numreverse(int n, F f) {
+  for (int i = 0; i < n; i++) {
+    int spaces = n - i - 1; // 3
+    int letters =  2 * i + 1;
+    for (int j = 0; j < spaces; j++) {
+      std::cout << " ";
+    }
+    // 1, 121, 12321, 1234321
+    for (int j = 1; j <= letters; j++) {
+      int mid = i + 1;
+      std::cout << f(abs(mid - j) + 1); // distance from mid + 1(offset since we're printing 1)
+    }
+    std::cout << "\n";
+  }
+}
+void pattern17_numreverse(int n) {
+    pattern17_numreverse(n, [](int x) {
+        return x;
+    });
+}
+
 void pattern20(int n) {
   int size = 2 * n; // size of the diamond in length
   for (int i = 1; i < size; i++) {
@@ -467,6 +558,100 @@ void diamond_pattern(int n) {
       std::cout<< "\n";
     }
   }
+  void triangle_hollow_pattern(int n) {
+    int stars, spaces;
+    for (int i = 0; i < n; i++) {
+      stars = i + 1;
+      spaces = n - i - 1;
+      for (int j = 0;  j < spaces; j ++) {
+        std::cout<< " ";
+      }
+      for (int j = 0;  j < stars; j ++) {
+        if (i == n - 1 || j == 0 || j == stars - 1) std::cout<< "*";
+        else std::cout<< " ";
+        if (j != stars - 1) std::cout << " ";
+      }
+      std::cout<< "\n";
+    }
+  }
+
+  void pattern11_repeat(int n) {
+    for (int i = 0; i < n; i++) {
+      for (int j = 0;  j <= i; j ++) {
+        std::cout<< (j + i + 1) % 2;
+      }
+      std::cout<< "\n";
+    }
+  }
+  void pattern12_repeat(int n) {
+    for (int i = 0; i < n; i++) {
+      for (int j = 0;  j < 2*n; j ++) {
+        if (j <= i) {
+          std::cout << j + 1;  
+        } else if (j >= 2*n - i - 1) {
+          std::cout << 2 * n - j;
+        } else {
+          std::cout << " ";
+        }
+      }
+      std::cout<< "\n";
+    }
+  }
+  void pattern13_repeat(int n) {
+    for (int i = 0; i < n; i++) {
+      // triangle number sum of last i numbers
+      int starting_number = (i * (i + 1)) / 2;
+      for (int j = 0;  j <= i; j++) {
+          std::cout << starting_number + j + 1 << " ";
+      }
+      std::cout<< "\n";
+    }
+  }
+  void pattern21_repeat(int n) {
+    for (int i = 0; i < n; i++) {
+      for (int j = 0;  j < n; j++) {
+        if ( i == 0 || i == n - 1 || j == 0 || j == n -1) std::cout << "*";
+        else std::cout << " ";
+      } 
+      std::cout<< "\n";
+    }
+  }
+  void pattern22_repeat_my_soln(int n) {
+    // map grid to a score, whichever score is higher according to the grid
+    // co-ordinates gets printed, otherwise it's just a normal square with
+    // 2 * n -1 side length
+    // even with this solution problem is represented as the distance to the center
+    // from any point so coordinate - center basically + 1 to offset if we say center is 0
+    // but we output 1
+    for (int i = 0; i < 2*n - 1; i++) {
+      for (int j = 0;  j < 2*n - 1; j++) {
+        int max_score, i_score, j_score;
+        if(i < n) i_score = n - i;
+        else i_score = i - n + 2;
+        if(j < n) j_score = n - j;
+        else j_score = j - n + 2;
+        max_score = std::max(i_score, j_score);
+        std::cout << max_score;
+      } 
+      std::cout<< "\n";
+    }
+  }
+
+  
+template <typename F>
+void pattern18_p(int n, F f) {
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j <= i; j++) {
+      std::cout << f(n + j - i);
+    }
+    std::cout << "\n";
+  }
+}
+void pattern18_p(int n) {
+    return pattern18_p(n, [](int x) {
+      return x;
+    });
+  }
 void print_string(std::string a){
   std::cout << a;
 }
@@ -510,6 +695,9 @@ int main() {
       // pattern17(num);
       // pattern18(num);
       // pattern19(num);
+      // print_string("--------------------------------\n");
+      // pattern19_gridzero_based(num);
+      // pattern20_gridzero_based(num);
       // pattern19_pointydiamond(num);
       // pattern20(num);
       // pattern21(num);
@@ -518,8 +706,21 @@ int main() {
       // diamond_pattern(num);
       // pattern2_reverse(num);
       // pattern5_reverse(num);
-      pattern8_hollow(num);
-      pattern8_hollow_rightsideup(num);
+      // pattern8_hollow(num);
+      // pattern8_hollow_rightsideup(num);
+      // triangle_hollow_pattern(num);
+      // pattern11_repeat(num);
+      // pattern12_repeat(num);
+      // pattern13_repeat(num);
+      // pattern21_repeat(num);
+      // pattern22_repeat_my_soln(num);
+      // pattern17_practice_numbers_version(num);
+      // pattern17_practice_numbers_version(num, [](int x) {
+      //   return static_cast<char>('A' + x - 1);
+      // });
+      pattern18_p(num, [](int x) {
+        return static_cast<char>('A' + x - 1);
+      });
       } else {
       std::cout << "Invalid input\n";
       break;
